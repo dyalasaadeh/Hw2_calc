73b85a759d4a2276d95003d54a22f3f4c643baef
USER <USER@3578-i7-SSD-8th>
2020-02-28 21:35:10 +0200

    goooooooooooo

---
+++ src/Test/to_Test.java
@@ -0,0 +1,52 @@
+package Test;
+
+import org.junit.Test;
+
+import calculator.calculator;
+import junit.framework.Assert;
+@SuppressWarnings("deprecation")
+public class to_Test {
+
+	class testcase {
+		@Test
+		public void testSum_BothNumbersArePositive_ShouldReturnPositiveNumber() {
+		// Arrange
+		int a = 10;
+		int b = 20;
+		calculator calc = new calculator();
+		// Act
+		int result = calc.add(a, b);
+		// Assert
+		Assert.assertTrue(result > 0);}
+		
+		
+		@Test
+		public void testSum_BothNumbersAreNegative_ShouldReturnNegativeNumber() {
+		// Arrange
+		int a = -10;
+		int b = -20;
+		calculator calc = new calculator();
+		// Act
+		int result = calc.add(a, b);
+		// Assert
+		Assert.assertTrue(result < 0);
+		
+		}
+		
+		@Test
+		public void testSum_BothNumbersAreEqualAndOpositeSign_ShouldReturnzero() {
+		// Arrange
+		int a = -10;
+		int b = 10;
+		calculator calc = new calculator();
+		// Act
+		int result = calc.add(a, b);
+		// Assert
+		Assert.assertTrue(result == 0);
+		
+		}
+
+	}
+
+
+}
